ea639f1d33c27f86acbab02290dcf25d187f035d
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-22 15:18:34 +0100

    Aufgabe 3

--- src/zweitespackage/Zahlenraten.java
+++ src/zweitespackage/Zahlenraten.java
@@ -4,28 +4,46 @@ import javax.swing.*;
 import java.util.Random;
 
 public class Zahlenraten {
-    public static void main(String[] args)
-    {
-    int min = 1;
-    int max = 10;
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 10;
 
-    Random rnd = new Random();
-    int zufallszahl = rnd.nextInt(max-min+1)+min;
-    //einen Zufallswert im Zahlenbereich min-max
+        Random rnd = new Random();
+        int zufallszahl = rnd.nextInt(max - min + 1) + min;
+        //einen Zufallswert im Zahlenbereich min-max
 
-    System.out.println(zufallszahl); //Ausgabe der Zufallszahl
 
-    if(zufallszahl%2==0) //Wenn die Zahl keinen Rest hat ist sie gerade
+
+    /*if(zufallszahl%2==0) //Wenn die Zahl keinen Rest hat ist sie gerade
     {
         JOptionPane.showMessageDialog(null, "Dies ist eine gerade Zahl");
-        System.out.println("Dies ist eine gerade Zahl");
+        JOptionPane.showMessageDialog(null, zufallszahl);
     }
     else{
         JOptionPane.showMessageDialog(null, "Dies ist eine ungerade Zahl");
+        JOptionPane.showMessageDialog(null, zufallszahl);
+    }*/
+
+
+    //String zahl = JOptionPane.showInputDialog("Geben Sie eine Zahl zwischen 1 und 10 ein");
+    //funktioniert so nicht weil ich Stringdatentypen nicht mit dem Integerdatentyp vergleichen kann
+
+    int zahl = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl zwischen 1 und 10 ein"));
+    //String Datentyp wird in Intgeger umgewandelt
+
+    int ergebnis = zahl-zufallszahl;
+
+    if(zahl==zufallszahl)
+    {
+        JOptionPane.showMessageDialog(null, "Das ist die richtige Zahl");
+    }
+    else if (ergebnis<=2&&ergebnis>=-2) {
+        JOptionPane.showMessageDialog(null, "Du bist schon sehr nah dran");
     }
 
+    else {
+        JOptionPane.showMessageDialog(null, "Das ist die falsche Zahl");
+        JOptionPane.showMessageDialog(null, "Die richtige Zahl war: " + zufallszahl);
+        }
     }
-
-
-
 }
