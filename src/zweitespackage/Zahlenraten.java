6376733bbcd34a31922a523b9209e4257b465b2e
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-25 14:24:21 +0100

    Aufgabe 2

--- src/zweitespackage/Zahlenraten.java
+++ src/zweitespackage/Zahlenraten.java
@@ -1,4 +1,22 @@
 package zweitespackage;
 
+import javax.swing.*;
+import java.util.Random;
+
 public class Zahlenraten {
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 10;
+
+        Random rnd = new Random();
+        int zufallszahl = rnd.nextInt(max-min+1)+min;
+
+        if (zufallszahl%2==0)
+        {
+            JOptionPane.showMessageDialog(null,  zufallszahl +" = gerade");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, zufallszahl + " = ungerade");
+        }
+    }
 }
